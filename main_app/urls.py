e9ea916e76d04378462c390fb61fff9739aae8d5
mlmirk <mike.l.mirkovic@gmail.com>
2021-11-21 17:09:04 -0500

    details page spun up

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.home, name='home'),
-    path("about/", views.about, name='about')
+    path("about/", views.about, name='about'),
+    path("finches/", views.finches_index , name="finches_index"),
+    path("finches/<int:finch_id>", views.finches_detail, name="finches_detail"),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.utils.regex_helper import flatten_result
-# Create your views here.
+from .models import Finch
+
+#Create your views here.
+
+
+
 
 def home(request):
-  return HttpResponse('<h1>Hello finchees</h1>')
+  return render(request,'home.html')
 
 def about(request):
-  return HttpResponse('<h1>About Finches</ht>')
+  return render(request, 'about.html')
+
+def finches_index(request):
+  finches= Finch.objects.all()
+  return render(request, 'finches/index.html', {'finches':finches})
+
+def finches_detail(request,finch_id):
+  finch=Finch.objects.get(id=finch_id)
+  return render(request,'finches/detail.html', {'finch' : finch} )
